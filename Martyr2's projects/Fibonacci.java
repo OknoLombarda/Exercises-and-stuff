11a70dc0ae54b0745436ed4a58dbe9781121afb9
Vadim <viouor@gmail.com>
2019-03-12 20:02:47 +0600

    Add Fibonacci Number program

---
+++ Martyr2's projects/Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fibonacci
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Type a number: ");
+		int n = in.nextInt();
+		System.out.println("\n0\n1");
+		long[] num = new long[n];
+		num[0] = 0;
+		num[1] = 1;
+		for (int i = 2; i < n; i++)
+		{
+			num[i] = num[i - 1] + num[i - 2];
+			System.out.println(num[i]);
+		}
+	}
+}
